6c6ac36d3a76ca493abe826465464ab1061a79a9
srinskit <srinskit@users.noreply.github.com>
2019-09-20 13:56:29 +0530

    Added error handlers

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 # Script to install nitkcaa
+[ $# -lt 2 ] && { echo "Usage: $0 username password [service-folder]"; exit 1; }
 
 username=$1
 password=$2
 ping_host="1.1.1.1"
 
+if [ $3 ]; then
+    service_root=$3
+else
+    service_root="/etc/systemd/system"
+fi
+
+service_path=$service_root"/nitkcaa.service"
 conf_path=$PWD"/nitkcaa.conf"
-service_path=$PWD"/nitkcaa.service"
-service_root="/etc/systemd/system""/nitkcaa.service"
+unit_path=$PWD"/nitkcaa.service"
 
 python_path=$(which python3)
 
@@ -15,8 +22,9 @@ $username
 $password
 $ping_host
 EOF
+[ $? -ne 0 ] && { echo "Could not generate config file."; exit 1; }
 
-cat << EOF > $service_path
+cat << EOF > $unit_path
 # nitkcaa.service
 
 [Unit] 
@@ -29,10 +37,14 @@ ExecStart= $python_path $PWD/main.py $conf_path service
 [Install]
 WantedBy=multi-user.target
 EOF
+[ $? -ne 0 ] && { echo "Could not generate service unit file."; exit 1; }
 
-ln -s $service_path $service_root
+ln -s $unit_path $service_path
+[ $? -ne 0 ] && { echo "Could not generate link in service folder."; exit 1; }
 systemctl enable nitkcaa.service
+[ $? -ne 0 ] && { echo "Could not enable service."; exit 1; }
 systemctl start nitkcaa.service
+[ $? -ne 0 ] && { echo "Could not start service."; exit 1; }
 
 echo -e "Add the following alias to your .rc file\n"
 echo alias nitkcaa=\"$python_path $PWD/main.py $conf_path oneoff\"
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,9 @@
 service_root="/etc/systemd/system""/nitkcaa.service"
 
 systemctl stop nitkcaa.service
+[ $? -ne 0 ] && { echo "Could not stop the service."; exit 1; }
 systemctl disable nitkcaa.service
+[ $? -ne 0 ] && { echo "Could not disable the service."; exit 1; }
 
 rm nitkcaa.conf nitkcaa.service
+[ $? -ne 0 ] && { echo "Could not cleanup."; exit 1; }
